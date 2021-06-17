c75e88b51aee724504f25600b2f5bb23a3d9ba36
Elana Korngold <elana.korngold@gmail.com>
2021-06-17 09:21:45 -0400

    added img to content

--- js/show-data.js
+++ js/show-data.js
@@ -1,8 +1,10 @@
 const data = {
   recipes:
-    "<h1>Recipes</h1> <p>Find and share everyday cooking inspiration on here. Discover recipes, cooks, videos, and how-tos based on the food you love and the friends you follow. Get the best recipes & how to content from Elana's Recipes.</p>",
+    "<h1>Recipes</h1><p>Find and share everyday cooking inspiration on here. Discover recipes, cooks, videos, and how-tos based on the food you love and the friends you follow. Get the best recipes & how to content from Elana's Recipes.</p><p><img src='img/cherrycake.png'/>",
 
-  reviews: "<h1>Reviews</h1> <p>Write reviews and add rating of recipies.<p>",
+  reviews:
+    "<h1>Reviews</h1><p>Write reviews and add rating of recipies.</p><p><img src='img/review.png'/></p>",
 
-  delivery: "<h1>Delivery</h1> <p>We deliver. Nobody bits our chef</p>",
+  delivery:
+    "<h1>Delivery</h1> <div><p>We deliver. Nobody bits our chef!</p><p><img src='img/chef.png'/></p>",
 };
